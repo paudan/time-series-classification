44e770a67b564b90bd4121cdd1ee7c67a2cbe223
Paulius <danpaulius@gmail.com>
2019-11-19 23:56:45 +0200

    Fix catch22 compilation

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,6 +4,7 @@ python3 -m pip install cython
 python3 -m pip install -r requirements.txt
 git clone https://github.com/chlubba/catch22.git
 cd catch22/wrap_Python
+export CFLAGS='-std=c99'
 python3 setup_P3.py build
 python3 setup_P3.py install
 cd ../..
