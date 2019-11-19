b25a7a8db62e30fa01f67260ad4222f3defa685c
paulius <danpaulius@gmail.com>
2019-11-19 23:45:17 +0200

    Shell script to install prerequisites

---
+++ install_requirements.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+python3 -m pip install cython
+python3 -m pip install -r requirements.txt
+git clone https://github.com/chlubba/catch22.git
+cd catch22/wrap_Python
+python3 setup_P3.py build
+python3 setup_P3.py install
+cd ../..
